c29800056dc361997dc282a35fcda6b17ff7fdf0
Neil Loknath <neil.loknath@gmail.com>
2010-01-11 09:31:35 -0700

    Re-write updatePhoto method for Android 2.x
    Need to update RawContact and set to super primary.

--- src/com/nloko/android/syncmypix/contactutils/ContactProxy2.java
+++ src/com/nloko/android/syncmypix/contactutils/ContactProxy2.java
@@ -25,12 +25,16 @@ package com.nloko.android.syncmypix.contactutils;
 import java.io.InputStream;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.Data;
+import android.provider.ContactsContract.RawContacts;
 
-public class ContactProxy2 extends ContactProxy {
+public class ContactProxy2 implements IContactProxy {
 
-	@Override
 	public InputStream getPhoto(ContentResolver cr, String id) {
 		if (cr == null || id == null) {
 			return null;
@@ -39,4 +43,79 @@ public class ContactProxy2 extends ContactProxy {
 		Uri contact = Uri.withAppendedPath(Contacts.CONTENT_URI, id);
 		return Contacts.openContactPhotoInputStream(cr, contact);
 	}
+	
+	public void updatePhoto(ContentResolver cr, byte[] photo, String id) { 
+		if (cr == null || id == null) {
+			return;
+		}
+		
+		long rawId = queryForRawContactId(cr, Long.parseLong(id));
+		if (rawId < 0) {
+			return;
+		}
+		
+		id = String.valueOf(rawId);
+		ContentValues values = new ContentValues(); 
+		int photoRow = -1; 
+		
+		// query for existing photo
+		String where = ContactsContract.Data.RAW_CONTACT_ID + " == " + 
+			id + " AND " + Data.MIMETYPE + "=='" + 
+			ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE + "'"; 
+		
+		Cursor cursor = cr.query(ContactsContract.Data.CONTENT_URI, null, where, null, null); 
+		int idIdx = cursor.getColumnIndexOrThrow(ContactsContract.Data._ID); 
+		if(cursor.moveToFirst()){ 
+			photoRow = cursor.getInt(idIdx); 
+		} 
+		cursor.close();
+		if (photoRow < 0 && photo == null) {
+			return;
+		}
+		
+		values.put(ContactsContract.Data.RAW_CONTACT_ID, 
+				id); 
+		values.put(ContactsContract.Data.IS_SUPER_PRIMARY, 1); 
+		values.put(ContactsContract.CommonDataKinds.Photo.PHOTO, photo); 
+		values.put(ContactsContract.Data.MIMETYPE, 
+				ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE); 
+		// append CALLER_IS_SYNCADAPTER to prevent sync
+		Uri.Builder builder = ContactsContract.Data.CONTENT_URI.buildUpon();
+		builder.appendQueryParameter(ContactsContract.CALLER_IS_SYNCADAPTER, "true");
+		
+		if(photoRow >= 0){ 
+			if (photo == null) {
+				cr.delete(builder.build(), ContactsContract.Data._ID 
+					+ " = " + photoRow, null);
+			} else {
+				cr.update 
+					(builder.build(), values, ContactsContract.Data._ID 
+					+ " = " + photoRow, null);
+			}
+		} else { 
+			cr.insert(builder.build(), values); 
+		} 
+	} 
+	
+	private long queryForRawContactId(ContentResolver cr, long contactId) {
+        Cursor rawContactIdCursor = null;
+        long rawContactId = -1;
+        
+        if (cr != null) {
+	        try {
+	            rawContactIdCursor = cr.query(RawContacts.CONTENT_URI,
+	                    new String[] {RawContacts._ID},
+	                    RawContacts.CONTACT_ID + "=" + contactId, null, null);
+	            if (rawContactIdCursor != null && rawContactIdCursor.moveToFirst()) {
+	                // Just return the first one.
+	                rawContactId = rawContactIdCursor.getLong(0);
+	            }
+	        } finally {
+	            if (rawContactIdCursor != null) {
+	                rawContactIdCursor.close();
+	            }
+	        }
+        }
+        return rawContactId;
+    }
 }
