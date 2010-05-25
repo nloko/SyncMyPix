b35aeff339fa0bbd85aff62538109370e73edb30
Neil Loknath <neil.loknath@gmail.com>
2010-05-25 10:30:09 -0600

    Do caching asynchronously

--- src/com/nloko/android/PhotoCache.java
+++ src/com/nloko/android/PhotoCache.java
@@ -38,7 +38,12 @@ import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Bitmap.CompressFormat;
+import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.os.Process;
 
 public class PhotoCache {
 	// this location is important, as it allows automatic removal when the app is
@@ -47,7 +52,11 @@ public class PhotoCache {
 	public long mMaxBytes = 5000000;
 	
 	private static final String TAG = "PhotoCache";
-	
+	private static final int DELETE_ALL = 1;
+	private static final int DELETE = 2;
+	private static final int ADD = 3;
+	private static final int SHUTDOWN = 4;
+		
 	private BroadcastReceiver mExternalStorageReceiver;
 	private WeakReference<Context> mContext;
 	private boolean mExternalStorageAvailable;
@@ -57,6 +66,8 @@ public class PhotoCache {
 	private final TreeMap<Long, List<String>> mPhotos = new TreeMap<Long, List<String>>();
 	private long mSize = 0;
 	private boolean mSized = false;
+	
+	private final AsyncHandler mHandler;
 	
 	public PhotoCache(Context context, long maxBytes) {
 		this(context);
@@ -72,12 +83,28 @@ public class PhotoCache {
 		File path = Environment.getExternalStorageDirectory();
 		mPath = new File(path, String.format(BASE_DIR, context.getPackageName()));
 		startWatchingExternalStorage();
+		
+		HandlerThread thread = new HandlerThread("PhotoCacheThread", 
+				Process.THREAD_PRIORITY_BACKGROUND);
+		thread.start();
+		mHandler = new AsyncHandler(thread);
 	}
 
 	private void ensurePath() {
 		if (mExternalStorageWriteable) {
 			mPath.mkdirs();
 		}
+	}
+	
+	public void delete(String name) {
+		Message msg = mHandler.obtainMessage();
+		msg.what = DELETE;
+		msg.obj = name;
+		mHandler.sendMessage(msg);
+	}
+
+	public void deleteAll() {
+		mHandler.sendEmptyMessage(DELETE_ALL);
 	}
 	
 	private synchronized void resize() {
@@ -128,40 +155,13 @@ public class PhotoCache {
 	}
 	
 	public void releaseResources() {
+		mHandler.sendEmptyMessage(SHUTDOWN);
 		stopWatchingExternalStorage();
 	}
 	
-	public void destory() {
+	public void destroy() {
 		deleteAll();
-		stopWatchingExternalStorage();
-	}
-	
-	public synchronized void deleteAll() {
-		File[] files = mPath.listFiles();
-		if (files == null) {
-			return;
-		}
-		
-		for(File f : files) {
-			if (mExternalStorageWriteable) {
-				if (mSized) {
-					mSize -= f.length();
-				}
-				f.delete();
-			}
-		}
-	}
-	
-	public synchronized void delete(String name) {
-		if (!mExternalStorageWriteable || name == null) {
-			return;
-		}
-		
-		File f = new File(mPath, name);
-		if (f.exists()) {
-			mSize -= f.length();
-			f.delete();
-		}
+		releaseResources();
 	}
 	
 	public Bitmap get(String file) {
@@ -172,37 +172,12 @@ public class PhotoCache {
 		File path = new File(mPath, file);
 		return BitmapFactory.decodeFile(path.getAbsolutePath());
 	}
-	
-	public synchronized void add(String file, Bitmap bitmap) {
-		if (bitmap == null) {
-			throw new IllegalArgumentException("bitmap");
-		}
-		if (file == null) {
-			throw new IllegalArgumentException("file");
-		}
-		
-		if (!mSized) {
-			calculateSize();
-		}
-		
-		File photo = new File(mPath, file);
-		if (photo.exists()) {
-			return;
-		}
-		
-		if (mExternalStorageWriteable) {
-			try {
-				ensurePath();
-				OutputStream os = new FileOutputStream(photo);
-				bitmap.compress(CompressFormat.JPEG, 100, os);
-				os.close();
-				updateSize(photo);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+
+	public void add(String file, Bitmap bitmap) {
+		Message msg = mHandler.obtainMessage();
+		msg.what = ADD;
+		msg.obj = new Photo(file, bitmap);
+		mHandler.sendMessage(msg);
 	}
 	
 	private void startWatchingExternalStorage() {
@@ -253,6 +228,108 @@ public class PhotoCache {
 		    // Something else is wrong. It may be one of many other states, but all we need
 		    //  to know is we can neither read nor write
 		    mExternalStorageAvailable = mExternalStorageWriteable = false;
+		}
+	}
+	
+	
+	private final class AsyncHandler extends Handler { 
+		public AsyncHandler(HandlerThread t) {
+			super(t.getLooper());
+		}
+
+		private synchronized void delete(String name) {
+			if (!mExternalStorageWriteable || name == null) {
+				return;
+			}
+			
+			File f = new File(mPath, name);
+			if (f.exists()) {
+				mSize -= f.length();
+				f.delete();
+			}
+		}
+	
+		private synchronized void deleteAll() {
+			File[] files = mPath.listFiles();
+			if (files == null) {
+				return;
+			}
+			
+			for(File f : files) {
+				if (mExternalStorageWriteable) {
+					if (mSized) {
+						mSize -= f.length();
+					}
+					f.delete();
+				}
+			}
+		}
+
+		private synchronized void add(String file, Bitmap bitmap) {
+			if (bitmap == null) {
+				throw new IllegalArgumentException("bitmap");
+			}
+			if (file == null) {
+				throw new IllegalArgumentException("file");
+			}
+			
+			if (!mSized) {
+				calculateSize();
+			}
+			
+			File photo = new File(mPath, file);
+			if (photo.exists()) {
+				return;
+			}
+			
+			if (mExternalStorageWriteable) {
+				try {
+					ensurePath();
+					OutputStream os = new FileOutputStream(photo);
+					bitmap.compress(CompressFormat.JPEG, 100, os);
+					os.close();
+					updateSize(photo);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			
+			switch(msg.what) {
+			case ADD:
+				Photo p = (Photo)msg.obj;
+				if (p != null) {
+					add(p.file, p.bitmap);
+				}
+				break;
+			case DELETE_ALL:
+				deleteAll();
+				break;
+				
+			case DELETE:
+				delete((String)msg.obj);
+				break;
+
+			case SHUTDOWN:
+				getLooper().quit();
+				break;
+			}
+		}
+	};
+	
+	private static final class Photo {
+		public String file;
+		public Bitmap bitmap;
+		
+		public Photo(String f, Bitmap b) {
+			file = f;
+			bitmap = b;
 		}
 	}
 }
