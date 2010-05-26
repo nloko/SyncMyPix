2f3a60bd6a1c072adbeee998773d19fd43a56c0e
Neil Loknath <neil.loknath@gmail.com>
2010-05-26 10:24:28 -0600

    Add debuging into to confirmContact

--- src/com/nloko/android/syncmypix/contactutils/ContactProxy2.java
+++ src/com/nloko/android/syncmypix/contactutils/ContactProxy2.java
@@ -51,18 +51,22 @@ public class ContactProxy2 implements IContactProxy {
 	}
 	
 	public PhoneContact confirmContact(ContentResolver cr, String id, String lookup) {
+		String newId = null;
+		String newLookup = null;
+		
 		Uri lookupUri = Contacts.getLookupUri(Long.parseLong(id), lookup);
 		Cursor c = cr.query(lookupUri, new String[] { Contacts._ID, Contacts.LOOKUP_KEY }, null, null, null);
 		try {
 			if (c.moveToFirst()) {
-				id = c.getString(c.getColumnIndex(Contacts._ID));
-				lookup = c.getString(c.getColumnIndex(Contacts.LOOKUP_KEY));
+				Log.d(TAG, "confirmContact() lookup success");
+				newId = c.getString(c.getColumnIndex(Contacts._ID));
+				newLookup = c.getString(c.getColumnIndex(Contacts.LOOKUP_KEY));
 			}
 		} finally {
 			c.close();
 		}
 		
-		return new PhoneContact(id, null, lookup);
+		return newId == null ? null : new PhoneContact(newId, null, newLookup);
 	}
 	
 	public void updatePhoto(ContentResolver cr, byte[] photo, String id, boolean markDirty) { 
