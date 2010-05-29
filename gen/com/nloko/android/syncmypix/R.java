af063b2612e65aa5fce5342481e06d57b5920631
Neil Loknath <neil.loknath@gmail.com>
2010-05-28 20:56:29 -0600

    Remove old background, polish sync & cancel buttons

--- gen/com/nloko/android/syncmypix/R.java
+++ gen/com/nloko/android/syncmypix/R.java
@@ -18,24 +18,23 @@ public final class R {
     }
     public static final class drawable {
         public static final int about_button=0x7f020000;
-        public static final int background=0x7f020001;
-        public static final int camera_crop_height=0x7f020002;
-        public static final int camera_crop_width=0x7f020003;
-        public static final int cancel_button_default=0x7f020004;
-        public static final int cancel_button_pressed=0x7f020005;
-        public static final int cancel_button_selector=0x7f020006;
-        public static final int default_face=0x7f020007;
-        public static final int icon=0x7f020008;
-        public static final int indicator_autocrop=0x7f020009;
-        public static final int logo=0x7f02000a;
-        public static final int mini_icon=0x7f02000b;
-        public static final int neutral_face=0x7f02000c;
-        public static final int results_button=0x7f02000d;
-        public static final int sad_face=0x7f02000e;
-        public static final int settings_button=0x7f02000f;
-        public static final int smiley_face=0x7f020010;
-        public static final int sync_button=0x7f020011;
-        public static final int title=0x7f020012;
+        public static final int camera_crop_height=0x7f020001;
+        public static final int camera_crop_width=0x7f020002;
+        public static final int cancel_button_default=0x7f020003;
+        public static final int cancel_button_pressed=0x7f020004;
+        public static final int cancel_button_selector=0x7f020005;
+        public static final int default_face=0x7f020006;
+        public static final int icon=0x7f020007;
+        public static final int indicator_autocrop=0x7f020008;
+        public static final int logo=0x7f020009;
+        public static final int mini_icon=0x7f02000a;
+        public static final int neutral_face=0x7f02000b;
+        public static final int results_button=0x7f02000c;
+        public static final int sad_face=0x7f02000d;
+        public static final int settings_button=0x7f02000e;
+        public static final int smiley_face=0x7f02000f;
+        public static final int sync_button=0x7f020010;
+        public static final int title=0x7f020011;
     }
     public static final class id {
         public static final int ImageView01=0x7f07002a;
